02992a9c7b752493acfae1188ac287bbc26e0c30
Joseph Nguyen <joseph@teksoft1.com>
2020-09-07 14:54:07 +0700

    change the project name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('farhal-online-angular app is running!');
+    expect(page.getTitleText()).toEqual('online-request app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'farhal-online-angular';
+  title = 'online-request';
 }
