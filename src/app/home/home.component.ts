79fa4e4a4bee164b57f877023a8f6599fb82796f
Joseph Nguyen <joseph@teksoft1.com>
2020-09-08 11:53:02 +0700

    remove duplicated code by using angular elements

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component } from '@angular/core';
-import * as scripts from './scripts.js';
+// import * as scripts from './scripts.js';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -11,17 +11,88 @@ export class HomeComponent implements AfterViewInit {
   constructor(private translate: TranslateService) {}
   public language = 'en';
 
-  private static createGoogleMapsScript(): void {
-    if (document.getElementById('google-apis')) {
-      return;
-    }
-    const script = document.createElement('script');
-    script.src =
-      'https://maps.googleapis.com/maps/api/js?sensor=false&key=AIzaSyBHpAmimxTn6JfSP_-1PavnZ9WvAE6eCtc&libraries=places&callback=initAutocomplete';
-    script.async = true;
-    script.defer = true;
-    script.id = 'google-apis';
-    document.body.insertAdjacentElement('beforeend', script);
+  public steps = [
+    'mainDiv',
+    'ftDiv',
+    'ftDiv2',
+    'serDiv1',
+    'ftDiv3',
+    'ftDiv4',
+    'sdDiv',
+    'tdDiv',
+    'lastDiv',
+    'lastDiv2',
+    'lastDiv3',
+    'lastDiv4',
+  ];
+
+  public currentStep = 'mainDiv';
+
+  // private static createGoogleMapsScript(): void {
+  //   if (document.getElementById('google-apis')) {
+  //     return;
+  //   }
+  //   const script = document.createElement('script');
+  //   script.src =
+  //     'https://maps.googleapis.com/maps/api/js?sensor=false&key=AIzaSyBHpAmimxTn6JfSP_-1PavnZ9WvAE6eCtc&libraries=places&callback=initAutocomplete';
+  //   script.async = true;
+  //   script.defer = true;
+  //   script.id = 'google-apis';
+  //   document.body.insertAdjacentElement('beforeend', script);
+  // }
+
+  private showPreviousButtonScreens = [
+    // 'mainDiv',
+    'ftDiv',
+    'ftDiv2',
+    // 'serDiv1',
+    'ftDiv3',
+    'ftDiv4',
+    'sdDiv',
+    'tdDiv',
+    'lastDiv',
+    'lastDiv2',
+    'lastDiv3',
+    'lastDiv4',
+  ];
+
+  private showNextButtonScreens = [
+    'mainDiv',
+    'ftDiv',
+    'ftDiv2',
+    // 'serDiv1', --search
+    'ftDiv3',
+    'ftDiv4',
+    'sdDiv',
+    'tdDiv',
+    'lastDiv',
+    'lastDiv2',
+    'lastDiv3',
+    'lastDiv4',
+  ];
+
+  public searchRequest(): void {
+    this.currentStep = 'ftDiv3';
+  }
+
+  public isNextButtonShowed(): boolean {
+    return this.showNextButtonScreens.includes(this.currentStep);
+  }
+
+  public isPreviousButtonShowed(): boolean {
+    return this.showPreviousButtonScreens.includes(this.currentStep);
+  }
+
+  public next(): void {
+    this.currentStep = this.steps[this.getCurrentStepIndex() + 1];
+  }
+
+  private getCurrentStepIndex(): number {
+    return this.steps.findIndex((x) => x === this.currentStep);
+  }
+
+  public previous(): void {
+    this.currentStep = this.steps[this.getCurrentStepIndex() - 1];
   }
 
   public changeLanguage(e: Event): void {
@@ -40,10 +111,10 @@ export class HomeComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     try {
-      scripts();
+      // scripts();
     } catch (e) {
       console.warn(e);
     }
-    setTimeout(HomeComponent.createGoogleMapsScript);
+    // setTimeout(HomeComponent.createGoogleMapsScript);
   }
 }
