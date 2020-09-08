296dc0b3951c14038ea91937b024693f55f25b83
Joseph Nguyen <joseph@teksoft1.com>
2020-09-08 09:32:38 +0700

    add change language

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,41 @@
-import {AfterViewInit, Component} from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import * as scripts from './scripts.js';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'ot-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements AfterViewInit {
-
-  constructor() {
-  }
+  constructor(private translate: TranslateService) {}
+  public language = 'en';
 
   private static createGoogleMapsScript(): void {
     if (document.getElementById('google-apis')) {
       return;
     }
     const script = document.createElement('script');
-    script.src = 'https://maps.googleapis.com/maps/api/js?sensor=false&key=AIzaSyBHpAmimxTn6JfSP_-1PavnZ9WvAE6eCtc&libraries=places&callback=initAutocomplete';
+    script.src =
+      'https://maps.googleapis.com/maps/api/js?sensor=false&key=AIzaSyBHpAmimxTn6JfSP_-1PavnZ9WvAE6eCtc&libraries=places&callback=initAutocomplete';
     script.async = true;
     script.defer = true;
     script.id = 'google-apis';
     document.body.insertAdjacentElement('beforeend', script);
+  }
+
+  public changeLanguage(e: Event): void {
+    e.preventDefault();
+    this.setLanguage();
+    this.translate.use(this.language);
+  }
+
+  private setLanguage(): void {
+    if (this.language === 'en') {
+      this.language = 'ar';
+    } else {
+      this.language = 'en';
+    }
   }
 
   ngAfterViewInit(): void {
@@ -31,5 +46,4 @@ export class HomeComponent implements AfterViewInit {
     }
     setTimeout(HomeComponent.createGoogleMapsScript);
   }
-
 }
