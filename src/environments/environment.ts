c98fd23ec82f0804577ecae9a5fc8de0cb6f0aea
Joseph Nguyen <joseph@teksoft1.com>
2020-09-15 17:21:47 +0700

    add more configuration for Test server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,8 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:5900',
   licenseUrl: 'http://localhost:5000',
+  licenseUser: 'sale003@gmail.com',
+  licensePassword: '123456',
 };
 
 /*
